0dbf03beda0da4753aa2bf71cdfc562dc205010e
Lucas Sodré <lucas10maglhaessodre@gmail.com>
2022-02-26 17:55:21 -0300

    Creation of the card skills component

---
+++ src/components/CardsSkills/CardsSills.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+
+import styles from './styles.module.scss';
+
+interface CardSkillsProps{
+    srcImg: string;
+    altImg: string;
+    titleCard: string;
+    descriptionCard: string;
+}
+export function CardSkills({
+  srcImg, altImg, titleCard, descriptionCard,
+}:CardSkillsProps) {
+  return (
+
+    <div className={styles.container}>
+
+      <div className={styles.image}>
+        <Image
+          src={`/${srcImg}`}
+          alt={altImg}
+          width={65}
+          height={60}
+        />
+
+      </div>
+
+      <h1>{titleCard}</h1>
+
+      <p>{descriptionCard}</p>
+
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import { AboutMe } from '../components/AboutMe';
 import { Banner } from '../components/Banner';
+// import { CardSkills } from '../components/CardsSkills/CardsSills';
 import { Menu } from '../components/Menu';
+import { Skills } from '../components/Skills/Skills';
+// import logo from '../../public/html.png';
 
 export default function Home() {
   return (
@@ -8,6 +11,7 @@ export default function Home() {
       <Menu />
       <Banner />
       <AboutMe />
+      <Skills />
     </>
   );
 }
