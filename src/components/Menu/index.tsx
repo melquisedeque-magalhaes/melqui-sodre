078438911963c3a243ba0e4881920921716bd310
Melqui <melqui.sodre15@gmail.com>
2022-06-13 19:31:05 -0300

    ajuste no header

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -3,45 +3,48 @@ import { Spin as Hamburger } from 'hamburger-react';
 import { useMediaQuery } from 'react-responsive';
 
 import { Link } from '../Link';
+import { ModalMenuMobile } from './ModalMenuMobile';
+
 import styles from './styles.module.scss';
 
+
 export function Menu() {
-  const isWideVersion = useMediaQuery({ query: '(min-width: 1280px)' });
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const isWideVersion = useMediaQuery({ query: '(min-width: 1024px)' });
 
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+    function handleChangeStateModal() {
+        setIsMenuOpen(oldValue => !oldValue)
+    }
 
-  if (isWideVersion) {
+    if (isWideVersion) {
+        return (
+            <div className={styles.header}>
+                <Link href="#sobre-mim">Sobre Mim</Link>
+                <Link href="#sobre-mim">Tecnologias</Link>
+                <Link href="#sobre-mim">Projetos</Link>
+                <Link href="#sobre-mim">Contatos</Link>
+            </div>
+        );
+    }
+
     return (
-      <div className={styles.header}>
-        <Link href="#sobre-mim">Sobre Mim</Link>
-        <Link href="#sobre-mim">Tecnologias</Link>
-        <Link href="#sobre-mim">Projetos</Link>
-        <Link href="#sobre-mim">Contatos</Link>
-      </div>
-    );
-  }
+        <>
+            <div className={styles.menuMobile}>
 
-  return (
-    <>
-      <div className={styles.menuMobile}>
-        <div className={styles.buttonMenuContainer}>
-          <Hamburger color="#fff" toggled={isMenuOpen} toggle={setIsMenuOpen} />
-        </div>
-
-      </div>
-
-      {
-                isMenuOpen && (
-                <div className={styles.menuMobileFull}>
-                  <div className={styles.navigator}>
-                    <Link href="#sobre-mim">Sobre Mim</Link>
-                    <Link href="#sobre-mim">Tecnologias</Link>
-                    <Link href="#sobre-mim">Projetos</Link>
-                    <Link href="#sobre-mim">Contatos</Link>
-                  </div>
+                <div className={styles.buttonMenuContainer}>
+                    <Hamburger color="#fff" toggled={isMenuOpen} toggle={setIsMenuOpen} />
                 </div>
+
+            </div>
+
+            {
+                isMenuOpen && (
+                    <ModalMenuMobile
+                        isOpenMenuMobile={isMenuOpen}
+                        handleCloseModal={handleChangeStateModal}
+                    />
                 )
             }
-    </>
-  );
+        </>
+    );
 }
